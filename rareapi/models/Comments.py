c9a947bc2349bcf64839f90a1ab51cac5ca169f6
mgjeffries <gib.jeffries@gmail.com>
2020-11-09 13:49:15 -0600

    fix error in foreign key name

--- rareapi/models/Comments.py
+++ rareapi/models/Comments.py
@@ -2,8 +2,7 @@ from django.db import models
 
 class Comments(models.Model):
     post = models.ForeignKey("Posts", on_delete=models.CASCADE)
-    author = models.ForeignKey("RareUser", on_delete=models.CASCADE)
+    author = models.ForeignKey("RareUsers", on_delete=models.CASCADE)
     content = models.CharField(max_length=300)
     subject = models.CharField(max_length=100)
     created_on = models.DateTimeField()
-    
