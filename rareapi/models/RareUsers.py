a2650ef0e25d608969916e9bdb59749644d1e76e
mgjeffries <gib.jeffries@gmail.com>
2020-11-09 13:49:46 -0600

    update rare user created on to date time

--- rareapi/models/RareUsers.py
+++ rareapi/models/RareUsers.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 class RareUsers(models.Model):
     bio = models.CharField(max_length=300)
     profile_image_url = models.CharField(max_length=100)
-    created_on = models.DateField()
+    created_on = models.DateTimeField()
     active = models.BooleanField(default=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
