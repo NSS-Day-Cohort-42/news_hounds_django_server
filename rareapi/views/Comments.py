fc9f3857c87799feed7534232ee4ecb493ce5ae0
FORDBA <bfordcpa@gmail.com>
2020-11-12 11:03:04 -0600

    Delete Comment

--- rareapi/views/Comments.py
+++ rareapi/views/Comments.py
@@ -67,6 +67,15 @@ class CommentViewSet(ViewSet):
             comments, many=True, context={'request': request})
         return Response(serializer.data)
 
+    def destroy(self, request, pk=None):
+        try:
+            comment = Comments.objects.get(pk=pk)
+            comment.delete()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        except Comments.DoesNotExist as ex:
+            return Response({'message': ex.args[0]})
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 
