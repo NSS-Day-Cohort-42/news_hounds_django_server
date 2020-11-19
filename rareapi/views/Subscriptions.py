51a13229b13eb9fdc75d347895e4c046840d1c89
Michael-Tyler <miketylerabc@gmail.com>
2020-11-19 12:44:43 -0500

    added the put action to update the subscription object

--- rareapi/views/Subscriptions.py
+++ rareapi/views/Subscriptions.py
@@ -1,6 +1,7 @@
 """ View Module FOr handling requests about subscriptions"""
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework import status
 from django.utils import timezone
 from rareapi.models import RareUsers, Subscriptions
@@ -36,22 +37,64 @@ class SubscriptionsViewSet(ViewSet):
             return Response({}, status=status.HTTP_201_CREATED)
         except Exception as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
-    def partial_update(self, request, pk=None):
-        """Handle PATCH request"""
-            # author_id = request.data["author_id"]
-            # djangoUser = request.auth.user
-            # follower = RareUsers.objects.get(user=djangoUser)
-# Get the subscription based off it's primary key
-        try:
-            subscription = Subscriptions.objects.get(follower=follower, author=author, ended_on=None)
-        except Subscriptions.DoesNotExist:
-            return Response({'message': 'There is no Subscription with this given ID'}, status=status.HTTP_400_BAD_REQUEST)
-# Next update the request with the updated 
+    """action to update the ended_on date for when someone unsubscribes"""
+    @action(methods=['put'], detail=False)
+    def update_subscription(self,request):
+        #First gather all the data needed from the request
+        author_id = request.data["author_id"]
+        #this is the author
+        author = RareUsers.objects.get(pk=author_id)
+        djangoUser = request.auth.user
+        #this is the follower (the person logged in and Unsubscribing)
+        follower = RareUsers.objects.get(user=djangoUser)
+        #next get the subscriptions
+        subscription=Subscriptions.objects.get(author=author, follower=follower,ended_on=None)
+        
         subscription.ended_on = timezone.now()
-
-        try: 
+        try:
             subscription.save()
-            return Response({}, status=status.HTTP_204_NO_CONTENT)
+            return Response({}, status=status.HTTP_201_CREATED)
         except Exception as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     def partial_update(self, request, pk=None):
+#         """Handle PATCH request"""
+#             # author_id = request.data["author_id"]
+#             # djangoUser = request.auth.user
+#             # follower = RareUsers.objects.get(user=djangoUser)
+# # Get the subscription based off it's primary key
+#         try:
+#             subscription = Subscriptions.objects.get(follower=follower, author=author, ended_on=None)
+#         except Subscriptions.DoesNotExist:
+#             return Response({'message': 'There is no Subscription with this given ID'}, status=status.HTTP_400_BAD_REQUEST)
+# Next update the request with the updated 
+#         subscription.ended_on = timezone.now()
+
+#         try: 
+#             subscription.save()
+#             return Response({}, status=status.HTTP_204_NO_CONTENT)
+#         except Exception as ex:
+#             return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
